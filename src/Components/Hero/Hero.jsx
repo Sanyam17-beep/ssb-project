0f012ab02db4bb181153b63ee8ca88e7b1c3ae60
Sanyam17-beep <sanyamsharma590128@gmail.com>
2023-12-26 20:10:41 +0530

    images added

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -5,6 +5,8 @@ import { SlMouse } from "react-icons/sl";
 import Anidat from "../../Anidat.json";
 import { FaPlay } from "react-icons/fa6";
 import AnchorLink from "react-anchor-link-smooth-scroll";
+import im1 from "../../im1.jpg";
+import im2 from "../../im2.jpg";
 function Hero() {
   const defaultOptions = {
     loop: true,
@@ -26,12 +28,9 @@ function Hero() {
           <div className="Top-footer">
             Expert Coaching for Success in Defence Entrance Exams
           </div>
-        </div>
-        <div className="Image-container1"></div>
-      </div>
-      <div className="para-container">
+          <div className="para-container">
         <p className="para">
-          At [Tutoring Service Name], we excel in guiding candidates toward
+          At SSB BullsEye, we excel in guiding candidates toward
           success in SSB & NDA exams. With experienced coaches and a proven
           track record, our tailored approach ensures comprehensive preparation.
           We equip you with skills and confidence needed to conquer these
@@ -40,26 +39,42 @@ function Hero() {
         <div className="get-container">
           <div className="getintouch">Get in touch</div>
           <div className="watch-container">
-              <div className="watch-video">
-               <div className="play-icon">
+            <div className="watch-video">
+              <div className="play-icon">
                 <FaPlay />
               </div>
               <div>Watch Our Video</div>
-               </div>
-                 <div  className="arrow">
-            <div  className="lottie"> <Lottie options={defaultOptions} height={100} width={150} /></div> 
-            <div className="Explore-circle">Explore all modules</div>
-                </div>
-               
-                 </div>
+            </div>
+            <div className="arrow">
+              <div className="lottie">
+                {" "}
+                <Lottie options={defaultOptions} height={100} width={150} />
+              </div>
+              <div className="Explore-circle">Explore all modules</div>
+            </div>
+          </div>
         </div>
       </div>
-      <div className="Image-container2"></div>
-      <AnchorLink href="#box" style={{textDecoration:"none",color:"black"}} >
-      <div className="scroll-container">
-      <SlMouse style={{fontSize:"25px"}} className="Mouseicon"/>
-       <div style={{height:"fit-content"}}> <div className="scroll">SCROLL</div></div>
+        </div>
+        <div className="Image-container1">
+          <img src={im1} alt="" className="image" />
+        </div>
       </div>
+  
+      <div className="Image-container2">
+        <img src={im2} alt="" className="image" />
+      </div>
+      <AnchorLink
+        href="#box"
+        style={{ textDecoration: "none", color: "black" }}
+      >
+        <div className="scroll-container">
+          <SlMouse style={{ fontSize: "20px" }} className="Mouseicon" />
+          <div style={{ height: "fit-content" }}>
+            {" "}
+            <div className="scroll">SCROLL</div>
+          </div>
+        </div>
       </AnchorLink>
     </div>
   );
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,11 +1,14 @@
 import React,{ useState } from 'react'
 import "./style.css"
+import svglogo from "../../Primarygreen.svg";
 function Navbar() {
   
   const [Active,SetActive]=useState("Home");
   return (
     <div className='Nav'>
-      <div className='logo'>logo</div>
+      <div className='logo'>
+       <img src={svglogo} alt="" width={120}/>
+      </div>
       <div className='menu'>
         <div style={Active=="Home"?{cursor:"pointer",fontWeight:"700"}:{cursor:"pointer"}} onClick={()=>SetActive("Home")}>Home</div>
         <div style={Active=="About us"?{cursor:"pointer",fontWeight:"700"}:{cursor:"pointer"}} onClick={()=>SetActive("About us")}>About us</div>
